be0b06d03dec5b419f1e2fe692253f2dcea28786
BernatBC <bernat.borrasc@gmail.com>
2023-03-11 18:29:09 +0100

    Translate

--- 2-Prolog/exercici3.pl
+++ 2-Prolog/exercici3.pl
@@ -1,14 +1,14 @@
 % 3. Representando conjuntos con listas sin repeticiones, escribe predicados para las operaciones de intersección y unión de conjuntos dados.
-unio([],[],[]):-!.
-unio([],L,L):-!.
-unio(L,[],L):-!.
-unio([X1|L1],L2,[X1|U]):- unio(L1,L2,U), not(conte(X1,U)).
-unio([X1|L1],L2,U):-unio(L1,L2,U), conte(X1,U).
+union([],[],[]):-!.
+union([],L,L):-!.
+union(L,[],L):-!.
+union([X1|L1],L2,[X1|U]):- union(L1,L2,U), not(contains(X1,U)).
+union([X1|L1],L2,U):-union(L1,L2,U), contains(X1,U).
 
-interseccio(_,[],[]):-!.
-interseccio([],_,[]):-!.
-interseccio([X|L1],L2,[X|I]):-interseccio(L1,L2,I), conte(X,L2).
-interseccio([X|L1],L2,I):-interseccio(L1,L2,I), not(conte(X,L2)).
+intersection(_,[],[]):-!.
+intersection([],_,[]):-!.
+intersection([X|L1],L2,[X|I]):-intersection(L1,L2,I), contains(X,L2).
+intersection([X|L1],L2,I):-intersection(L1,L2,I), not(contains(X,L2)).
 
-conte(X,[X|_]):-!.
-conte(X,[_|L]):-conte(X,L).
+contains(X,[X|_]):-!.
+contains(X,[_|L]):-contains(X,L).
