fac93dc7ca9705ff61135775400926fe4ca8489a
BernatBC <bernat.borrasc@gmail.com>
2023-03-12 13:14:52 +0100

    Redo base case

--- 2-Prolog/exercici5.pl
+++ 2-Prolog/exercici5.pl
@@ -1,4 +1,4 @@
 % 5. Escribe un predicado fib(N,F) que signifique: “F es el N-ésimo número de Fibonacci para la N dada”. Estos números se definen ası́: fib(1) = 1, fib(2) = 1, y si N > 2 entonces fib(N ) = fib(N − 1) + fib(N − 2).
-fib(0,R):-R is 0, !.
-fib(1,R):-R is 1, !.
+fib(0,0):-!.
+fib(1,1):-!.
 fib(N,R):-A is N - 1, B is N - 2, fib(A,F1), fib(B,F2), R is F1 + F2.
