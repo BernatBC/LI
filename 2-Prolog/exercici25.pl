319da5fc31ef3ee09319850013fbeed13002acf9
BernatBC <bernat.borrasc@gmail.com>
2023-03-22 19:00:29 +0100

    Add statement

--- 2-Prolog/exercici25.pl
+++ 2-Prolog/exercici25.pl
@@ -1,3 +1,4 @@
+% 25. Complete the following predicate in prolog.
 % nthRoot( N, K, R ) === "Given positive integers N and K, the integer part of the Nth root of K is R".
 % Example: the integer part of the 2th root (square root) of 16 is 4.
 % Example: the integer part of the 3rd root (cubic root) of 8 is 2.
